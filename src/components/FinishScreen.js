14f2ec5ce64f313a812dd058fae26ffc0e23e5a0
Faith Makinde <faithmakinde0@gmail.com>
2023-07-25 21:27:50 +0100

    added the emoji functionality

--- src/components/FinishScreen.js
+++ src/components/FinishScreen.js
@@ -11,7 +11,7 @@ export default function FinishScreen({points, maxPossiblePoints, highScore, disp
  
   return (
    <>
-      <p className='result'>You scored <strong>{points}</strong> out of {maxPossiblePoints} ({Math.ceil(percentage)}%)</p>
+      <p className='result'>You scored <strong>{points}</strong> out of {maxPossiblePoints} ({Math.ceil(percentage)}%){emoji}</p>
       <p className='highscore'>(Highscore: {highScore}points)</p>
       <button className='btn btn-ui' onClick={() => dispatch({type: "restart"})}>Restart quiz</button>
       </>
