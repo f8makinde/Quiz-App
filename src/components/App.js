e213bbf2edc18cd0ecfaa350c26e7b90ecf3724a
Faith Makinde <faithmakinde0@gmail.com>
2023-07-27 15:05:52 +0100

    updated app

--- src/components/App.js
+++ src/components/App.js
@@ -92,7 +92,7 @@ const App = () => {
   const maxPossiblePoints = questions.reduce((prev, cur) => prev + cur.points, 0)
   useEffect(() => {
   
-      fetch("http://localhost:9000/questions")
+      fetch("/data/questions")
       .then ((res) => res.json())
       .then((data) => dispatch({type: "dataReceived", payload: data}))
      .catch((err) => dispatch({type: "dataFailed"}))
